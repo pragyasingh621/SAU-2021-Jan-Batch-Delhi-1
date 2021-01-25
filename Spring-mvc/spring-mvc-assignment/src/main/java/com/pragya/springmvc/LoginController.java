59d293556f56114d04889904dc4ff6e77c8fee7a
PRAGYA SINGH <pragyasingh621@gmail.com>
2021-01-25 15:56:24 +0530

    Spring-mvc-changes

--- Spring-mvc/spring-mvc-assignment/src/main/java/com/pragya/springmvc/LoginController.java
+++ Spring-mvc/spring-mvc-assignment/src/main/java/com/pragya/springmvc/LoginController.java
@@ -10,63 +10,64 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
-import org.json.simple.JSONObject;  
+import org.json.simple.JSONObject;
 
 @Controller
 public class LoginController {
 
-	private LoginService loginService = new LoginService();
-	
-	@RequestMapping(value = "/login",method = RequestMethod.GET)
-	
-	public String showLoginPage() {
-		return "login";
-	}
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String handleUserLogin(ModelMap model, @RequestParam String firstNum,
-			@RequestParam String secondNum, @RequestParam String operator)
-					throws Exception {
-		
-	
-        if(firstNum.isBlank() || secondNum.isBlank()) {
-      	  throw new NumberFormatException("Input is wrong");
+    private LoginService loginService = new LoginService();
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+
+    public String showLoginPage() {
+        return "login";
+    }
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String handleUserLogin(ModelMap model, @RequestParam String firstNum,
+        @RequestParam String secondNum, @RequestParam String operator)
+    throws Exception {
+
+
+        if (firstNum.isBlank() || secondNum.isBlank()) {
+            throw new NumberFormatException("Input is wrong");
         }
-	       
-         //Zero exception 
-		if(operator.equals("DIVIDE") && secondNum.equals("0")) {
-			JSONObject obj=new JSONObject();    
-			  obj.put("operation","/");
-			  obj.put("message", "divide by zero");
-			  model.put("msg", obj);
-			  System.out.println(obj);
-			  return "NullPointerException";
-		}
-		
-	
-		int result = loginService.calculate(firstNum, secondNum, operator);
-		
-		//Greater than zero
-		if(result>1000) {
-			throw new IOException("Large Output");
-		}
-		
-		else {
-		model.put("name", firstNum);
-		model.put("sname", secondNum);
-		model.put("oname", result);
-		return "welcome";
-		}
-	}
-	
-	
-	@ExceptionHandler(value=IOException.class)
-	public String handleNullPointException(Exception e) {
-		return "LargeNumber";
-	}
-	
-	
-	@ExceptionHandler(value=Exception.class)
-	public String handleGenericException(Exception e) {
-		return "GenericError";
-	}
+
+        //Zero exception 
+
+        if (operator.equals("DIVIDE") && secondNum.equals("0")) {
+            JSONObject obj = new JSONObject();
+            obj.put("operation", "/");
+            obj.put("message", "divide by zero");
+            model.put("msg", obj);
+            System.out.println(obj);
+            return "NullPointerException";
+        }
+
+
+        int result = loginService.calculate(firstNum, secondNum, operator);
+
+        //Greater than 1000
+
+        if (result > 1000) {
+            throw new IOException("Large Output");
+        } else {
+            model.put("name", firstNum);
+            model.put("sname", secondNum);
+            model.put("oname", result);
+            return "welcome";
+        }
+    }
+
+
+    @ExceptionHandler(value = IOException.class)
+    public String handleNullPointException(Exception e) {
+        return "LargeNumber";
+    }
+
+    //	Generic Exception
+
+    @ExceptionHandler(value = Exception.class)
+    public String handleGenericException(Exception e) {
+        return "GenericError";
+    }
 }
